05231be83ce0568fd1e9959f1a0f75566dbb9b3b
Kelsey H <kels-hall23@hotmail.com>
2021-11-12 09:50:26 -0500

    checking it worked

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Kelsey Hall's </h1>
+      </header>
     </div>
   );
 }
