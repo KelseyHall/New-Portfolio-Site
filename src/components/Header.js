e331b5f89b95341c340418e1709dd505119920ca
Kelsey H <kels-hall23@hotmail.com>
2021-11-18 10:44:09 -0500

    mobile nav functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,41 +4,119 @@ import {
   IconButton,
   Toolbar,
   Typography,
+  Drawer,
+  List,
+  ListItem,
 } from '@mui/material';
-import React from 'react';
+import { Menu } from '@mui/icons-material';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+  const DisplayMobile = () => {
+    const handleDrawerOpen = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: true }));
+    const handleDrawerClose = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: false }));
+    return (
+      <Toolbar>
+        <Typography variant="h2" className="Logo">
+          Kelsey Hall's
+        </Typography>
+        <IconButton
+          size="large"
+          edge="end"
+          color="secondary"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={handleDrawerOpen}
+        >
+          <Menu />
+        </IconButton>
+        <Drawer
+          sx={{
+            width: '40vw',
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: '40vw',
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="temporary"
+          anchor="right"
+          open={drawerOpen}
+          onClose={handleDrawerClose}
+        >
+          <div>
+            <List className="NavFont">
+              <ListItem className="Nav-item">
+                <NavLink to="/">Home</NavLink>
+              </ListItem>
+              <ListItem className="Nav-item">
+                <NavLink to="/gallery">Gallery</NavLink>
+              </ListItem>
+              <ListItem className="Nav-item">
+                <NavLink to="/projects">projects</NavLink>
+              </ListItem>
+              <ListItem className="Nav-item">
+                <NavLink to="/contactme">contact me</NavLink>
+              </ListItem>
+            </List>
+          </div>
+        </Drawer>
+      </Toolbar>
+    );
+  };
+
+  const DisplayDesktop = () => {
+    return (
+      <Toolbar>
+        <Typography variant="h2" className="Logo">
+          Kelsey Hall's
+        </Typography>
+
+        <ul className="NavFont Nav-List">
+          <li className="Nav-item">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li className="Nav-item">
+            <NavLink to="/gallery">Gallery</NavLink>
+          </li>
+          <li className="Nav-item">
+            <NavLink to="/projects">projects</NavLink>
+          </li>
+          <li className="Nav-item">
+            <NavLink to="/contactme">contact me</NavLink>
+          </li>
+        </ul>
+      </Toolbar>
+    );
+  };
+
+  const [state, setState] = useState({
+    mobileView: false,
+    drawerOpen: false,
+  });
+  const { mobileView, drawerOpen } = state;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+
+    setResponsiveness();
+    window.addEventListener('resize', () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener('resize', () => setResponsiveness());
+    };
+  }, []);
   return (
     <Container>
       <AppBar position="static" className="App-header">
-        <Toolbar>
-          <Typography variant="h2" className="Logo">
-            Kelsey Hall's
-          </Typography>
-          <IconButton
-            size="large"
-            edge="end"
-            color="secondary"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          ></IconButton>
-
-          <ul className="NavFont">
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/gallery">Gallery</NavLink>
-            </li>
-            <li>
-              <NavLink to="/projects">projects</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contactme">contact me</NavLink>
-            </li>
-          </ul>
-        </Toolbar>
+        {mobileView ? <DisplayMobile /> : <DisplayDesktop />}
       </AppBar>
     </Container>
   );
