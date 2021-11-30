80d94a4dea833e4d4ca8440b040903edb78e3149
Kelsey H <kels-hall23@hotmail.com>
2021-11-30 10:51:46 -0500

    added pop up content for projects

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -5,29 +5,70 @@ import {
   Card,
   CardContent,
   CardMedia,
-  CardActionArea,
   Container,
+  Button,
+  Box,
+  Modal,
 } from '@mui/material';
+import theme from '../StyleSheet/theme';
 
 import projects from '../components/projectsDatabase';
-
-const DisplayProjects = (item) => (
-  <Grid key={item.title} item xs={12} sm={6} md={4}>
-    <Card>
-      <CardActionArea>
-        <CardMedia component="img" height="140" image={item.img} alt="Temp" />
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '90%',
+  'max-height': '80%',
+  bgcolor: `${theme.palette.secondary.light}`,
+  border: `2px solid ${theme.palette.secondary.main}`,
+  boxShadow: 24,
+  p: 4,
+  overflow: 'scroll',
+};
+const DisplayProjects = ({ title, description, img, content }) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <Grid key={title} item xs={12} sm={6} md={6}>
+      <Card>
+        <CardMedia component="img" height="140" image={img} alt="Temp" />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {item.title}
+            {title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {item.description}
+            {description}
           </Typography>
+
+          <Button onClick={handleOpen}>See Project</Button>
+          <Modal open={open} onClose={handleClose}>
+            <Box sx={style}>
+              <Button
+                sx={{ color: theme.palette.secondary.main }}
+                onClick={handleClose}
+              >
+                close
+              </Button>
+              {content.map(({ title, imgs, src }) => (
+                <Box className="project-contents">
+                  <Typography gutterBottom variant="h5" component="div">
+                    {title}
+                  </Typography>
+                  {imgs.map((item) => (
+                    <img src={item} width="100%" alt={title} />
+                  ))}
+                  {src ? <Button>view live</Button> : null}
+                </Box>
+              ))}
+            </Box>
+          </Modal>
         </CardContent>
-      </CardActionArea>
-    </Card>
-  </Grid>
-);
+      </Card>
+    </Grid>
+  );
+};
 
 const ProjectsPage = () => {
   return projects.map(({ design, coding }) => (
@@ -67,3 +108,31 @@ const ProjectsPage = () => {
 };
 
 export default ProjectsPage;
+
+// const [show, setShow] = React.useState(false);
+// const container = React.useRef(null);
+
+// const handleClick = () => {
+//   setShow(!show);
+// };
+
+// <Button onClick={handleClick}>
+// {show ? 'Hide Project' : 'See Project'}
+// </Button>
+
+// {show ? (
+// <Portal container={container.current}>
+//   {content.map(({ title, imgs, src }) => (
+//     <Box className="project-contents">
+//       <Typography gutterBottom variant="h5" component="div">
+//         {title}
+//       </Typography>
+//       {imgs.map((item) => (
+//         <img src={item} width="20%" />
+//       ))}
+//       {src ? <Button>view live</Button> : ''}
+//     </Box>
+//   ))}
+// </Portal>
+// ) : null}
+// <Box ref={container} sx={{ width: '100vw' }} />
--- src/components/projectsDatabase.js
+++ src/components/projectsDatabase.js
@@ -1,42 +1,101 @@
 import tempImg from '../img/IMG_3475.jpeg';
 
+import businessCard1 from '../projects/businessCard-Front.png';
+import businessCard2 from '../projects/businessCard-Back.png';
+
 const projects = [
   {
     design: [
       {
-        title: 'DesignProject1',
+        title: 'beauty by Kaylene',
         description: 'about this project',
         img: tempImg,
+        content: [
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+          {
+            title: 'gift certificate',
+            imgs: [],
+            src: '',
+          },
+        ],
       },
       {
         title: 'DesignProject2',
         description: 'about this project',
         img: tempImg,
+        content: [],
       },
       {
         title: 'DesignProject3',
         description: 'about this project',
         img: tempImg,
+        content: [
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+        ],
       },
       {
         title: 'DesignProject4',
         description: 'about this project',
         img: tempImg,
+        content: [
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+        ],
       },
       {
         title: 'DesignProject5',
         description: 'about this project',
         img: tempImg,
+        content: [
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+        ],
       },
       {
         title: 'DesignProject6',
         description: 'about this project',
         img: tempImg,
-      },
-      {
-        title: 'DesignProject7',
-        description: 'about this project',
-        img: tempImg,
+        content: [
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+        ],
       },
     ],
     coding: [
@@ -44,36 +103,86 @@ const projects = [
         title: 'CodingProject1',
         description: 'about this project',
         img: tempImg,
+        content: [
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: 'button',
+          },
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+        ],
       },
       {
         title: 'CodingProject2',
         description: 'about this project',
         img: tempImg,
+        content: [
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+        ],
       },
       {
         title: 'CodingProject3',
         description: 'about this project',
         img: tempImg,
+        content: [
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+        ],
       },
       {
         title: 'CodingProject4',
         description: 'about this project',
         img: tempImg,
+        content: [
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+        ],
       },
       {
         title: 'CodingProject5',
         description: 'about this project',
         img: tempImg,
-      },
-      {
-        title: 'CodingProject6',
-        description: 'about this project',
-        img: tempImg,
-      },
-      {
-        title: 'CodingProject7',
-        description: 'about this project',
-        img: tempImg,
+        content: [
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+          {
+            title: 'business Card',
+            imgs: [businessCard1, businessCard2],
+            src: '',
+          },
+        ],
       },
     ],
   },
